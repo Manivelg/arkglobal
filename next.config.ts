bb43ca994c51fa4ee40d749abbce6c1d99bce253
Manivel Ganesan <manivel@cloudrevelinnovation.com>
2025-08-30 16:08:15 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   productionBrowserSourceMaps: false,
   images: {
-    domains: ["arkglobalworldwide.com"], // Add if using external images
+    domains: ["arkglobalworldwide.com"],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.arkglobalworldwide.com",
+          },
+        ],
+        destination: "https://arkglobalworldwide.com/:path*",
+        permanent: true,
+      },
+    ];
   },
 };
 
